add6c81a6298c5e5d5968cd7a512b81e9bd4b5e0
CKBosco <chankinbok@gmail.com>
2026-06-27 17:42:41 +0800

    Added background + sound and changed options

--- src/Menu/MainMenu.jsx
+++ src/Menu/MainMenu.jsx
@@ -1,21 +1,33 @@
 import styles from './Menu.module.css'
+import menu from '../assets/Menu.mp4'
+import hoverSound from '../assets/MenuHover.wav'
 export default function MainMenu() {
+    const playHover = () => {
+        const sound = new Audio(hoverSound);
+        sound.volume = 0.2;
+        sound.play();
+    };
     const options = [
-        { name: "SKILL", className: styles.skill },
-        { name: "ITEM", className: styles.item },
-        { name: "EQUIP", className: styles.equip },
-        { name: "PERSONA", className: styles.persona },
-        { name: "STATS", className: styles.stats },
-        { name: "QUEST", className: styles.quest },
+        { name: "PROFILE", className: styles.profile },
+        { name: "EDUCATION", className: styles.education },
+        // { name: "EQUIP", className: styles.equip },
+        { name: "SKILLS", className: styles.skills },
+        { name: "PROJECTS", className: styles.projects },
+        { name: "EXPERIENCE", className: styles.experience },
         { name: "CONFIG", className: styles.config }
     ];
     return (
         <div className={styles.mainContainer}>
+            <video
+                className={styles.videoBg}
+                src= {menu} 
+                autoPlay loop muted playsInline />
             <div className={styles.optionsContainer}>
                 {options.map((opt) => (
                     <div 
                         key={opt.name} 
                         className={`${styles.menuOption} ${opt.className}`}
+                        onMouseEnter={playHover} 
                     >
                         <span className={styles.textElement}>{opt.name}</span>
                     </div>
