edf60529b6a160fd719a738cbe14fd04e19a5af0
CKBosco <chankinbok@gmail.com>
2026-06-27 18:12:08 +0800

    Removed unrealted assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router";
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 // import './App.css'
 import MainMenu from './Menu/MainMenu.jsx'
 import Config from './Config/Config.jsx'
