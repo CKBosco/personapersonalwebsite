d70dd73edaeefdf3b0638cb8154566e4f8dcebc1
CKBosco <chankinbok@gmail.com>
2026-07-04 15:42:05 +0800

    Education page 95% done

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
 import styles from './Education.module.css';
 import { useTransitionNavigate } from '../Transition/useTransitionNavigate';
 import eduVd from '../assets/questM.mp4';
@@ -10,6 +11,17 @@ export default function Education() {
         sound.volume = 0.2;
         sound.play().catch(() => {});
     };
+    const uniDetails = {
+        degree: "BSc Computer Science and Artificial Intelligence",
+        date: "2024 - Present",
+        details: "Key Modules:\nComing soon"
+    }
+    const colDetails = {
+        degree: "A-Levels",
+        date: "2022-2024",
+        details: "Information Technology (A*)\nMathematics (A)\nChinese (A)\nEconomics (B)"
+    }
+    const [details, setDetails] = useState(uniDetails);
     return (
         <div className={styles.mainContainer}>
             <video
@@ -29,17 +41,22 @@ export default function Education() {
                         <div className={styles.schoolStatus}>Name</div>
                         <div className={styles.status}>Status</div>
                     </div>
-                    <div className={styles.schoolContainer}>
+                    {/* onMouseEnter */}
+                    <div className={styles.schoolContainer} onClick={() => setDetails(uniDetails)}>
                         <div className={styles.school}>Loughborough University</div>
-                        <div className={styles.progress}>In Progress</div>
+                        <div className={styles.inProgress}>InProgress</div>
+                    </div>
+                    <div className={styles.schoolContainer} onClick={() => setDetails(colDetails)}>
+                        <div className={styles.school}>Ratcliffe College</div>
+                        <div className={styles.completed}>Done</div>
                     </div>
                 </div>
                 <div className={styles.detailContainer}>
                     <div className={styles.degreeDateContainer}>
-                        <div className={styles.degree}>BSc Computer Science and Artificial Intelligence</div>
-                        <div className={styles.date}>2024 - Present</div>
+                        <div className={styles.degree}>{details.degree}</div>
+                        <div className={styles.date}>{details.date}</div>
                     </div>
-                    <div className={styles.details}>Details</div>
+                    <div className={styles.details}>{details.details}</div>
                 </div>
             </div>
         </div>
