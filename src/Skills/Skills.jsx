28610c4c04951616894fbeec58540c86aae72fb8
CKBosco <chankinbok@gmail.com>
2026-07-11 23:04:30 +0800

    Added text for skills page

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -24,9 +24,15 @@ export default function Skills() {
                 <span className={styles.textElement}>BACK</span>
             </div>
             <div className={styles.tsContainer}>
+                <div className={styles.tsTitle}>Technical Skills</div>
+                <div className={styles.plTitle}>Programming Languages</div>
+                <div className={styles.langList}>{`HTML, JavaScript, CSS, PHP, Java, C++\nPython, SQL, Haskell, Kotlin`}</div>
+                <div className={styles.fTitle}>Frameworks & Tools</div>
+                <div className={styles.fList}>{`Git, React, Laravel, VSCode, Jupyter Notebook\nEclipse IDE, Arduino, Android Studio`}</div>
             </div>
             <div className={styles.osContainer}>
-                
+                <div className={styles.osTitle}>Other Skills</div>
+                <div className={styles.osList}>{`Problem Solving\nCritical Thinking\nCommunication\nTeamwork\nAdaptability\nTime Management\nResponsible\nVideo editing\nGraphic design\nCustomer service experience`}</div>
             </div>
         </div>
     )
