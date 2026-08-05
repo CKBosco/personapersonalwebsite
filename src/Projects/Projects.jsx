460537530fc375c213d62b44f513f65a7a36996a
CKBosco <chankinbok@gmail.com>
2026-08-05 22:11:28 +0800

    Basic project overview

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import styles from './Projects.module.css';
 import { useTransitionNavigate } from '../Transition/useTransitionNavigate';
 import ProjVd from '../assets/skillsM.mp4';
 import hoverSound from '../assets/MenuHover.wav';
+import mitsuru from '../assets/projectCharacters/mitsuru.png';
 export default function Projects() {
     const navigate = useTransitionNavigate();
     const playHover = () => {
@@ -10,6 +11,16 @@ export default function Projects() {
         sound.volume = 0.2;
         sound.play().catch(() => {});
     };
+    const projects = [
+        {
+            id: 1,
+            name: "Project",
+            tools: "tools",
+            date: "date",
+            details: "details",
+            image: {mitsuru}
+        }
+    ]
     return (
         <div className={styles.mainContainer}>
             <video
@@ -22,6 +33,16 @@ export default function Projects() {
             onClick={() => navigate("/")}
             >
                 <span className={styles.textElement}>BACK</span>
+            </div>
+
+            <div className={styles.projectsContainer}>
+                {projects.map((pro) => (
+                    <div key={pro.id} className={styles.projectCard}>
+                        <img src={pro.image.mitsuru} alt={pro.name} className={styles.projectImage} />
+                        <div className={styles.projectName}>{pro.name}</div>
+                        <div className={styles.projectDate}>{pro.date}</div>
+                    </div>
+                ))}
             </div>
         </div>
     )
