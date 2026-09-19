e2ee36106ccc8f71b5f47d367a851a11871461cf
CKBosco <chankinbok@gmail.com>
2026-09-19 17:16:03 +0800

    Added responsive desgin for all pages

--- src/Config/Config.jsx
+++ src/Config/Config.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router";
+import {useState} from "react";
 import styles from './Config.module.css';
 import { useTransitionNavigate } from '../Transition/useTransitionNavigate';
 import config from '../assets/Config.mp4';
 import hoverSound from '../assets/MenuHover.wav';
 export default function Config({ soundOn, setSoundOn, playHover }) {
     const navigate = useTransitionNavigate();
+    const [videoOn, setVideoOn] = useState(true);
     // const playHover = () => {
     //     const sound = new Audio(hoverSound);
     //     sound.volume = 0.2;
@@ -34,6 +36,17 @@ export default function Config({ soundOn, setSoundOn, playHover }) {
                             ON
                         </button>
                         <button className={`${styles.toggleBtn} ${!soundOn ? styles.active : ''}`}>
+                            OFF
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.settingRow}>
+                    <div className={styles.settingLabel}>Video Background</div>
+                    <div className={styles.toggleContainer} onClick={() => setVideoOn(!videoOn)} onMouseEnter={playHover} >
+                        <button className={`${styles.toggleBtn} ${videoOn ? styles.active : ''}`}>
+                            ON
+                        </button>
+                        <button className={`${styles.toggleBtn} ${!videoOn ? styles.active : ''}`}>
                             OFF
                         </button>
                     </div>
