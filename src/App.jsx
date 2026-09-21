2efe75a880bbb4167a9cf7aff2dc52cbc74fb5b3
CKBosco <chankinbok@gmail.com>
2026-09-21 14:26:04 +0800

    Added base_url to fix github page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 // import './App.css'
 import VideoTransitionOverlay from './Transition/VideoTransitionOverlay.jsx';
 import MainMenu from './Menu/MainMenu.jsx';
